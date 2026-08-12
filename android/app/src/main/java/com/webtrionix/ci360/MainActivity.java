e1b254857d05e59d0b34b15216fa0e0408fe3498
Pradeep Verma <pradeepofficial8795@gmail.com>
2026-08-13 01:23:22 +0530

    Bump APK version and clear WebView cache

--- android/app/src/main/java/com/webtrionix/ci360/MainActivity.java
+++ android/app/src/main/java/com/webtrionix/ci360/MainActivity.java
@@ -1,5 +1,18 @@
 package com.webtrionix.ci360;
 
+import android.os.Bundle;
+import android.webkit.WebStorage;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        WebView webView = new WebView(this);
+        webView.clearCache(true);
+        webView.clearHistory();
+        webView.destroy();
+        WebStorage.getInstance().deleteAllData();
+        super.onCreate(savedInstanceState);
+    }
+}
